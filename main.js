982127bb2c5931d3893113d23d6a48eff7c1a2f0
SamuelBurnes <srburnes02@gmail.com>
2022-06-20 15:42:14 -0500

    Auth Test 1

--- main.js
+++ main.js
@@ -3,19 +3,35 @@ var fio_url = "https://fio.fnar.net";
 
 class MobileFIO
 {
+	var apikey;
 	loadInitial()
 	{
 		const url = window.location.href;
 		if(url.slice(0, 35) == "https://apex.prosperousuniverse.com")
 		{
-			console.log(localStorage.getItem('localTest'));
-			//localStorage.setItem('localTest', Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5));
-			console.log(localStorage.getItem('localTest'));
+			authenticate();
 		}
 		else
 		{
-			console.log(localStorage.getItem('localTest'));
 			window.setTimeout(() => this.loadInitial(), 1000);
+		}
+	}
+	
+	authenticate()
+	{
+		this.apikey = localStorage.getItem("fioinfo");
+		if(apikey == null || apikey == undefined)
+		{
+			// Wait for authenticate buffer
+			const container = document.getElementById("container");
+			try
+			{
+				const buffer = container.firstChild.firstChild.children[1].children[1].firstChild.firstChild;
+				if(buffer.firstChild.firstChild.textContent.toUpperCase().includes(" / XIT SETTINGS"))
+				{
+					console.log("Settings Screen");
+				}
+			} catch(error){}
 		}
 	}
 }
