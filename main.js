42e984b8fbc692e3d9450d02d22bddabc4257408
SamuelBurnes <srburnes02@gmail.com>
2022-06-20 20:00:24 -0500

    Auth Test 7

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var fnar_url = "https://rest.fnar.net";
 var fio_url = "https://fio.fnar.net";
 var apikey = null;
+var username = null;
 
 class MobileFIO
 {
@@ -19,55 +20,67 @@ class MobileFIO
 	
 	authenticate()
 	{
-		apikey = localStorage.getItem("fioinfo");
-		if(apikey == null || apikey == undefined)
+		var fiodata = JSON.parse(localStorage.getItem("fioinfo"));
+		username = fiodata[0];
+		apikey = fiodata[1];
+		// Wait for authenticate buffer
+		const container = document.getElementById("container");
+		try
 		{
-			// Wait for authenticate buffer
-			const container = document.getElementById("container");
-			try
+			const buffer = container.firstChild.firstChild.children[1].children[1].firstChild.firstChild;
+			if(buffer.firstChild.firstChild.textContent.toUpperCase().includes(" / XIT SETTINGS"))
 			{
-				const buffer = container.firstChild.firstChild.children[1].children[1].firstChild.firstChild;
-				console.log(buffer.firstChild.firstChild.textContent.toUpperCase());
-				if(buffer.firstChild.firstChild.textContent.toUpperCase().includes(" / XIT SETTINGS"))
+				const tile = buffer.children[1].firstChild;
+				tile.style.background = "#20314E";
+				tile.style.position = "static";
+				var namelabel;
+				var nameinput;
+				var apiinput;
+				var apilabel;
+				if(tile.children.length == 0)
 				{
-					console.log("Settings Screen");
-					const tile = buffer.children[1].firstChild;
-					tile.style.background = "#20314E";
-					tile.style.position = "static";
-					var namelabel;
-					var nameinput;
-					var apiinput;
-					var apilabel;
-					if(tile.children.length == 0)
-					{
-						namelabel = document.createElement("label");
-						namelabel.textContent = "FIO Username";
-						namelabel.style.fontSize = "30px";
-						tile.appendChild(namelabel);
-						nameinput = document.createElement("input");
-						nameinput.style.backgroundColor = "#42361d";
-						nameinput.style.borderBottom = "1px solid #8d6411";
-						tile.appendChild(nameinput);	
-						apilabel = document.createElement("label");
-						apilabel.textContent = "FIO API Key";
-						apilabel.style.fontSize = "30px";
-						tile.appendChild(apilabel);
-						apiinput = document.createElement("input");
-						apiinput.style.backgroundColor = "#42361d";
-						apiinput.style.borderBottom = "1px solid #8d6411";
-						tile.appendChild(apiinput);
-					}
-					else
-					{
-						namelabel = tile.children[0];
-						nameinput = tile.children[1];
-						apilabel = tile.children[2];
-						apiinput = tile.children[3];
-					}
+					namelabel = document.createElement("label");
+					namelabel.textContent = "FIO Username";
+					namelabel.style.fontSize = "14px";
+					namelabel.style.color = "#ffffff";
+					tile.appendChild(namelabel);
+					nameinput = document.createElement("input");
+					nameinput.style.backgroundColor = "#42361d";
+					nameinput.style.borderBottom = "1px solid #8d6411";
+					nameinput.style.padding = "2px";
+					nameinput.style.marginBottom = "5px";
+					nameinput.addEventListener("input", function(){
+						fiodata[0] = nameinput.value;
+						localStorage.set("fioinfo", JSON.stringify(fiodata));
+					});
+					if(username != null && username != undefined){nameinput.value = username;}
+					tile.appendChild(nameinput);	
+					apilabel = document.createElement("label");
+					apilabel.style.color = "#ffffff";
+					apilabel.textContent = "FIO API Key";
+					apilabel.style.fontSize = "14px";
+					tile.appendChild(apilabel);
+					apiinput = document.createElement("input");
+					apiinput.style.backgroundColor = "#42361d";
+					apiinput.style.borderBottom = "1px solid #8d6411";
+					apiinput.style.padding = "2px";
+					apiinput.addEventListener("input", function(){
+						fiodata[1] = apiinput.value;
+						localStorage.set("fioinfo", JSON.stringify(fiodata));
+					});
+					if(apikey != null && apikey != undefined){apiinput.value = apikey;}
+					tile.appendChild(apiinput);
 				}
-			} catch(error){console.error(error);}
-			if(apikey == null || apikey == undefined){window.setTimeout(() => this.authenticate(), 1000);}
-		}
+				else
+				{
+					namelabel = tile.children[0];
+					nameinput = tile.children[1];
+					apilabel = tile.children[2];
+					apiinput = tile.children[3];
+				}
+			}
+		} catch(error){console.error(error);}
+		window.setTimeout(() => this.authenticate(), 1000);
 	}
 }
 
